3af59f3cdd728162a215aa5f0f40b84caa7ae6a8
awchjimmy <awchjimmy@gmail.com>
2023-12-17 10:20:22 +0800

    fix #3, can run servers on osx

--- launch_login.sh
+++ launch_login.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
-export CLASSPATH=".:dist/odinms.jar:mina-core.jar:slf4j-api.jar:slf4j-jdk14.jar:mysql-connector-java-bin.jar"
-java -Dnet.sf.odinms.recvops=recvops.properties \
--Dnet.sf.odinms.sendops=sendops.properties \
--Dnet.sf.odinms.wzpath=. \
--Dnet.sf.odinms.login.config=login.properties \
--Djavax.net.ssl.keyStore=login.keystore \
--Djavax.net.ssl.keyStorePassword=loginkeystorepassword \
--Djavax.net.ssl.trustStore=login.truststore \
--Djavax.net.ssl.trustStorePassword=logintruststorepassword \
-net.sf.odinms.net.login.LoginServer
+export CLASSPATH=".:dist/XiuzSource.jar:dist/mina-core.jar:dist/slf4j-api.jar:dist/slf4j-jdk14.jar:dist/mysql-connector-java-bin.jar"
+java -Dnet.sf.odinms.recvops=recvops.properties -Dnet.sf.odinms.sendops=sendops.properties -Dnet.sf.odinms.wzpath=wz -Dnet.sf.odinms.login.config=login.properties -Djavax.net.ssl.keyStore=filename.keystore -Djavax.net.ssl.keyStorePassword=passwd -Djavax.net.ssl.trustStore=filename.keystore -Djavax.net.ssl.trustStorePassword=passwd net.sf.odinms.net.login.LoginServer
--- launch_world.sh
+++ launch_world.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
-export CLASSPATH=".:dist/odinms.jar:mina-core.jar:slf4j-api.jar:slf4j-jdk14.jar:mysql-connector-java-bin.jar"
-java -Dnet.sf.odinms.recvops=recvops.properties \
--Dnet.sf.odinms.sendops=sendops.properties \
--Dnet.sf.odinms.wzpath=. \
--Djavax.net.ssl.keyStore=world.keystore \
--Djavax.net.ssl.keyStorePassword=worldkeystorepassword \
--Djavax.net.ssl.trustStore=world.truststore \
--Djavax.net.ssl.trustStorePassword=worldtruststorepassword \
-net.sf.odinms.net.world.WorldServer
+export CLASSPATH=".:dist/XiuzSource.jar:dist/mina-core.jar:dist/slf4j-api.jar:dist/slf4j-jdk14.jar:dist/mysql-connector-java-bin.jar"
+java -Dnet.sf.odinms.recvops=recvops.properties -Dnet.sf.odinms.sendops=sendops.properties -Dnet.sf.odinms.wzpath=wz -Djavax.net.ssl.keyStore=filename.keystore -Djavax.net.ssl.keyStorePassword=passwd -Djavax.net.ssl.trustStore=filename.keystore -Djavax.net.ssl.trustStorePassword=passwd net.sf.odinms.net.world.WorldServer
